a8c485a7d671697db64262da181409680925f7b2
Xavier <chenchopan@gmail.com>
2018-07-27 20:08:58 +0800

    Initial Commit

---
+++ guessCardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guessCardApp
+//
+//  Created by Xavier Chen [MIGOTP] on 2018/7/27.
+//  Copyright © 2018年 Xavier Chen [MIGOTP]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
